90299e093eb2b234eca0c7d833c12895a475a2a4
Martin Ekberg <martin@mekberg.se>
2018-10-17 13:54:12 +0200

    Rewrite logic to make it simpler

--- wurtzisms/__main__.py
+++ wurtzisms/__main__.py
@@ -90,8 +90,7 @@ def refresh_cache(db):
         db.commit()
 
 
-def get_wurtzism():
-    db = init_cache(CACHE_PATH, CACHE_NAME)
+def get_wurtzism(db):
     c = db.cursor()
 
     random_note = c.execute("SELECT * FROM notes ORDER BY random() LIMIT 1").fetchone()[1]
@@ -106,23 +105,12 @@ def main():
     global args
     args = parser.parse_args()
 
+    db = init_cache(CACHE_PATH, CACHE_NAME)
+
     if args.endless:
         print('Press Ctrl+C to exit\n')
-        while True:
-            wurtzism = get_wurtzism()
-            if args.instant:
-                print(wurtzism)
-            else:
-                for char in wurtzism:
-                    print(char, end='', flush=True)
-                    # Randomize delay to simulate typing
-                    sleep_duration = random.randrange(1, 17, 1) / 100
-                    time.sleep(sleep_duration)
-                print()
-            print()
-            time.sleep(5)
-    else:
-        wurtzism = get_wurtzism()
+    while True:
+        wurtzism = get_wurtzism(db)
         if args.instant:
             print(wurtzism)
         else:
@@ -132,6 +120,10 @@ def main():
                 sleep_duration = random.randrange(1, 17, 1) / 100
                 time.sleep(sleep_duration)
             print()
+        if not args.endless:
+            return
+        print()
+        time.sleep(5)
 
 
 if __name__ == '__main__':
